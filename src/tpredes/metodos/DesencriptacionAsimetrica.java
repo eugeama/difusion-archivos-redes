b4cd620944595246e3d482d57f4e0ea801631ece
Eugeles <eufrales@gmail.com>
2025-10-31 11:47:17 -0300

    desencriptacion simetrica y asimetica listas

--- src/tpredes/metodos/DesencriptacionAsimetrica.java
+++ src/tpredes/metodos/DesencriptacionAsimetrica.java
@@ -2,22 +2,37 @@ package metodos;
 
 import javax.crypto.Cipher;
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.security.PublicKey;
 
 public class DesencriptacionAsimetrica {
     private LlavePubPriv parLlaves;
-
-    public static byte[] desencriptarAsimetricamenteDataArchivo(byte[] dataArchivo, PublicKey publicaEmisor) throws Exception{
+    private ReceptorArchivo receptorArchivo;
+/*
+    public byte[] desencriptarAsimetricamenteDataArchivo(byte[] dataArchivo, PublicKey publicaEmisor) throws Exception{
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, publicaEmisor);
 
         return cipher.doFinal(dataArchivo);
     }
 
-    public static byte[] desencriptarAsimetricamenteNombreArchivo(byte[]nombreArchivo, PublicKey publicaEmisor) throws Exception{
+    public byte[] desencriptarAsimetricamenteNombreArchivo(byte[]nombreArchivo, PublicKey publicaEmisor) throws Exception{
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, publicaEmisor);
 
         return cipher.doFinal(nombreArchivo);
     }
+*/
+    public void desencriptarAsimetricamenteArchivo(byte[]dataArchivo, byte[]nombreArchivo, PublicKey publicaEmisor) throws Exception{
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, publicaEmisor);
+
+        byte[] nombreArchivoDesencriptado = cipher.doFinal(nombreArchivo);
+        byte[] dataArchivoDesencriptado = cipher.doFinal(dataArchivo);
+
+        String nombreArchivoStr = new String(nombreArchivoDesencriptado, StandardCharsets.UTF_8);
+
+        receptorArchivo.guardarArchivo(nombreArchivoStr, dataArchivoDesencriptado, "ASIM");
+    }
+
 }
--- src/tpredes/metodos/DesencriptacionSimetrica.java
+++ src/tpredes/metodos/DesencriptacionSimetrica.java
@@ -2,8 +2,11 @@ package metodos;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class DesencriptacionSimetrica {
+    private ReceptorArchivo receptorArchivo;
 
     //desencriptacion clave aleatoria
     public SecretKey desencriptarClaveAleatoria(SecretKey claveAleatoria) throws Exception {
@@ -14,7 +17,7 @@ public class DesencriptacionSimetrica {
     }
 
     //desencriptacion del archivo con la clave aleatoria
-    public byte[] desencriptarSimetricamenteDataArchivo(byte[] dataArchivo, SecretKey claveAleatoria) throws Exception{
+    /*public byte[] desencriptarSimetricamenteDataArchivo(byte[] dataArchivo, SecretKey claveAleatoria) throws Exception{
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, claveAleatoria);
 
@@ -26,5 +29,17 @@ public class DesencriptacionSimetrica {
         cipher.init(Cipher.DECRYPT_MODE, claveAleatoria);
 
         return cipher.doFinal(nombreArchivo);
+    }*/
+
+    public void desencriptarSimetricamenteArchivo(byte[] nombreArchivo, byte[]dataArchivo, SecretKey claveAleatoria) throws Exception{
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, claveAleatoria);
+
+        byte[] nombreArchivoDesencriptado = cipher.doFinal(nombreArchivo);
+        byte[] dataArchivoDesencriptado = cipher.doFinal(dataArchivo);
+        String nombreArchivoStr = new String(nombreArchivoDesencriptado, StandardCharsets.UTF_8);
+
+        receptorArchivo.guardarArchivo(nombreArchivoStr, dataArchivoDesencriptado, "SIM");
+
     }
 }
